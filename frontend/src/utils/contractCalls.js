71b998bd5d5ee8c6dd0233502f9b8bfb9c8ab697
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-02-07 21:28:26 +0100

    feat(frontend): create postMessage contract call
    - Import Stacks connect and transactions
    - Define postMessage function
    - Configure contract call options
    - Handle transaction completion
    - Handle transaction cancellation
    - Foundation for contract interactions

---
+++ frontend/src/utils/contractCalls.js
@@ -0,0 +1,46 @@
+import { openContractCall } from '@stacks/connect'
+import {
+  uintCV,
+  stringUtf8CV,
+  PostConditionMode,
+} from '@stacks/transactions'
+import { getNetwork } from './network'
+import {
+  CONTRACT_ADDRESS,
+  CONTRACT_NAME,
+  FEE_POST_MESSAGE,
+  FEE_PIN_24HR,
+  FEE_PIN_72HR,
+  FEE_REACTION,
+  PIN_24HR_BLOCKS,
+  PIN_72HR_BLOCKS,
+} from './constants'
+
+export const postMessage = async (content, userSession) => {
+  const network = getNetwork()
+  
+  const functionArgs = [
+    stringUtf8CV(content),
+  ]
+
+  const options = {
+    network,
+    anchorMode: 1,
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: 'post-message',
+    functionArgs,
+    postConditionMode: PostConditionMode.Allow,
+    onFinish: (data) => {
+      console.log('Transaction ID:', data.txId)
+      return data.txId
+    },
+    onCancel: () => {
+      console.log('Transaction cancelled')
+    },
+  }
+
+  await openContractCall(options)
+}
+
+// TO BE CONTINUED - will add pin and react functions
